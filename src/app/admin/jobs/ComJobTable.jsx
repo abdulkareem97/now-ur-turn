99ed0f98c917e3e86504a93c7de57597449a75cc
abdulkareem97 <rajab8321@gmail.com>
2024-11-07 09:18:23 +0530

    version 1.3.3

--- src/app/admin/jobs/ComJobTable.jsx
+++ src/app/admin/jobs/ComJobTable.jsx
@@ -100,7 +100,7 @@ const ComJobTable = ({setRenderComponent}) => {
 
                                             dropDownOption.map((ele,ind)=>{
                                                 return (
-                                                    < li className="px-[12px] py-2  hover:text-[#87ceeb] active:text-[#00d8ff] cursor-pointer "
+                                                    <li className="px-[12px] py-2  hover:text-[#87ceeb] active:text-[#00d8ff] cursor-pointer "
                                                     onClick={() => {
                                                         setCom(ind)
                                                         setIsOpen(false)
