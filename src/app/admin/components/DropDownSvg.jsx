9f9edf9445a39ec46cd37d97bcc1675d78f24d56
abdulkareem97 <rajab8321@gmail.com>
2024-11-08 21:05:32 +0530

    version 1.3.9

--- src/app/admin/components/DropDownSvg.jsx
+++ src/app/admin/components/DropDownSvg.jsx
@@ -2,8 +2,8 @@
 const DropDownSvg = () => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
+        width="15"
+        height="15"
         viewBox="0 0 150 149.999998"
         preserveAspectRatio="xMidYMid meet"
         zoomAndPan="magnify"
